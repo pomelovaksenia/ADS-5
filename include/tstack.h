39282e1c633ef78e081c1e555f5940c16180713f
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-04-09 02:01:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,29 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+  T mass[size] = {0};
+  int a;
+
+ public:
+  TStack() {a = -1;}
+  void push(const T& value) {
+    if (full()) {
+      throw std::string("Empty!");
+    }
+    else {
+      mass[a++] = value;
+    }
+  }
+  T pol() const {return mass[a];}
+  T pop() {return mass[a--];}
+  int pri() const {return a;}
+  bool empty() const {return a == -1;}
+  bool full() const {return a == size;}
 };
 
 #endif  // INCLUDE_TSTACK_H_
